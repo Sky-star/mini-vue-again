2fb83bb679bf7ff2c82b773b790077b87c21dd13
白泉 <417941258@qq.com>
2023-08-25 10:25:40 +0800

    class的处理

--- packages/runtom-dom/src/index.ts
+++ packages/runtom-dom/src/index.ts
@@ -21,7 +21,11 @@ function shouldSetAsProps(el, key, value) {
 
 // 将属性设置相关的操作封装到patchProps函数中，并作为渲染器选项传递
 function patchProps(el, key, preValue, nextValue) {
-    if (shouldSetAsProps(el, key, nextValue)) {
+    // 出于对性能的考虑， 需要对 class 进行特殊的处理
+    // 另外的 style 也是类似的
+    if (key === 'class') {
+        el.className = nextValue || ''
+    } else if (shouldSetAsProps(el, key, nextValue)) {
         const type = typeof el[key]
         if (type === "boolean" && nextValue === "") {
             el[key] = true
