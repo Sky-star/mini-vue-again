4e22b91911f8b30ba6d5fd19c786717d5eeaade6
Sky-star <417941258@qq.com>
2023-09-26 23:27:19 +0800

    快速Diff算法-判断是否需要进行DOM移动操作

--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -226,6 +226,61 @@ export function createRenderer(options) {
             while (j < oldEnd) {
                 unmount(oldChildren[j++])
             }
+        } else {
+            // 构造 source 数组
+            // 新的一组子节点中剩余未处理节点的数量
+            const count = newEnd - j + 1
+            const source = new Array(count)
+            source.fill(-1)
+
+            // oldStart 和 newStart 分别为起始索引，即 j
+            const oldStart = j
+            const newStart = j
+            // moved变量代表是否需要移动节点
+            let moved = false
+            // pos 代表遍历过程中遇到的最大索引值
+            let pos = 0
+            // 构建索引表
+            const keyIndex = {}
+            for (let i = newStart; i <= newEnd; i++) {
+                keyIndex[newChildren[i].key] = i
+            }
+
+            // patched变量， 代表更新过的节点数量
+            let patched = 0
+            // 遍历旧的一组子节点中剩余未处理的节点
+            for (let i = oldStart; i <= oldEnd; i++) {
+                oldVNode = oldChildren[i]
+                // 如果跟新过的节点数量小于等于需要更新的节点数量，则执行循环
+                if (patched <= count) {
+                    // 通过索引表快速找到新的一组子节点中具有相同 key 值的节点位置
+                    const k = keyIndex[oldVNode.key]
+
+                    if (typeof k !== 'undefined') {
+                        newVNode = newChildren[k]
+                        // 调用 patch 函数完成更新
+                        patch(oldVNode, newVNode, container)
+                        // 每更新一个节点， 都将 patched 变量 + 1
+                        patched++
+                        // 填充 source 数组
+                        source[k - newStart] = i
+                        // 判断节点是否需要移动
+                        if (k < pos) {
+                            moved = true
+                        } else {
+                            pos = k
+                        }
+
+                    } else {
+                        // 没找到
+                        unmount(oldVNode)
+                    }
+                } else {
+                    // 如果更新过的节点数量大于需要更新的节点数量，则卸载多余的节点
+                    unmount(oldVNode)
+                }
+            }
+
         }
 
     }
