1e164644211ebc7286ee2496397cd11398c6d773
白泉 <417941258@qq.com>
2023-10-12 09:55:53 +0800

    异步组件-延迟与Loading组件

--- packages/runtime-core/src/apiAsyncComponent.ts
+++ packages/runtime-core/src/apiAsyncComponent.ts
@@ -2,9 +2,7 @@ import { ref } from "../../reactivity/src"
 
 // defineAsyncComponent 函数用于定义个异步组件， 接收一个异步组件加载作为参数
 export function defineAsyncComponent(options) {
-    // options 可以是配置项，也可以是加载器
-    if (typeof options === "function") {
-        // 如果 options 是加载器， 则将其格式化为配置项形式
+    if (typeof options === 'function') {
         options = {
             loader: options
         }
@@ -15,54 +13,58 @@ export function defineAsyncComponent(options) {
     let InnerComp = null
 
     return {
-        name: "AsyncComponentWrapper",
+        name: 'AsyncComponentWrapper',
         setup() {
             const loaded = ref(false)
-            // 定义 error，当错误发生时，用来存储错误对象
             const error = ref(null)
+            // 一个标志， 代表是否正在加载， 默认为 false
+            const loading = ref(false)
+
+            let loadingTimer: any = null
+            // 如果配置项中存才 delay， 则开启一个定时器计时，当延迟到后将 loading.value 设置为 true
+            if (options.delay) {
+                loadingTimer = setTimeout(() => {
+                    loading.value = true
+                }, options.delay)
+            } else {
+                // 如果配置项中没有 delay， 则直接标记为加载中
+                loading.value = true
+            }
 
             loader()
-                .then((c) => {
+                .then(c => {
                     InnerComp = c
                     loaded.value = true
                 })
-                .catch((err) => {
-                    // 添加 catch 语句来捕获加载过程中的错误
-                    err.vale = err
+                .catch((err) => error.value = err)
+                .finally(() => {
+                    loading.value = false
+                    // 加载完毕后，无论成功与否都要清除延迟定时器
+                    clearTimeout(loadingTimer)
                 })
 
             let timer: any = null
             if (options.timeout) {
-                // 如果指定了超时时长， 则开启一个定时器计时
                 timer = setTimeout(() => {
-                    // 超时后创建一个错误对象，并赋值给 error.value
-                    const err = new Error(`Async component timed out after ${options.timeout}ms.`)
+                    const err = new Error(`Async component time out after ${options.timeout}ms.`)
                     error.value = err
                 }, options.timeout)
             }
 
-            // 占位内容
-            const placeholder = { type: Text, children: "" }
+            const placeholder = { type: Text, children: '' }
 
             return () => {
-                if (loaded.value) {
-                    // 如果异步组件加载成功， 则渲染被加载的组件
+                if (loading.value) {
                     return { type: InnerComp }
                 } else if (error.value && options.errorComponent) {
-                    // 只有当错误存在且用户配置了 errorComponent 时才展示 Error 组件，同时将 error 作为 props 传递
-                    return options.errorComponent
-                        ? {
-                            type: options.errorComponent,
-                            props: {
-                                error: error.value
-                            }
-                        }
-                        : placeholder
+                    return { type: options.errorComponent, props: { error: error.value } }
+                } else if (loading.value && options.loadingComponent) {
+                    // 如果异步组件正在加载，并且用户指定了 Loading 组件，则渲染 Loading 组件
+                    return { type: options.loadingComponent }
+                } else {
+                    return placeholder
                 }
-
-                return placeholder
             }
         }
     }
 }
-
